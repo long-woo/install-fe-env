5f58ba710fb6ccb04bbd9aec38ac2ba189bf5ddc
long.woo <longwunet@outlook.com>
2021-05-17 21:56:07 +0800

    feat: 修改 readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # install-fe-env
+
 快速安装前端环境脚本。
+
+## 安装
+
+```sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/long-woo/install-fe-env/main/install-fe-env.sh)"
+```
--- install-fe-env.sh
+++ install-fe-env.sh
@@ -80,7 +80,7 @@ else
   npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs # phantomjs 二进制包镜像
   npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium # selenium 二进制包镜像
   npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector # node-inspector 二进制包镜像
-  npm set canvas_binary_host_mirror=https://npm.taobao.org/mirrors/canvas-prebuilt # node-canvas 二进制包镜像
+  npm set canvas_binary_host_mirror=https://npm.taobao.org/mirrors/node-canvas-prebuilt # node-canvas 二进制包镜像
   npm cache verify # 清空缓存
 fi
 
