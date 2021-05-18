baf69990c90e960e40e34b68bf97721ce46dcc11
long.woo <longwunet@outlook.com>
2021-05-18 21:29:14 +0800

    feat: 修改 npm 安装检查

--- install-fe-env.sh
+++ install-fe-env.sh
@@ -27,11 +27,26 @@ function command_exists() {
 
 # Homebrew 设置为阿里源
 function brewConfig() {
-  # 设置阿里源
   echo "# brew 阿里源 \n export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles" >> ~/.bash_profile
 
   # 修改后生效
   source ~/.bash_profile
+}
+
+# 设置淘宝源
+function npmConfig() {
+  npm set registry https://registry.npm.taobao.org # 注册模块镜像
+  npm set disturl https://npm.taobao.org/dist # node-gyp 编译依赖的 node 源码镜像
+  npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass # node-sass 二进制包镜像
+  npm set electron_mirror https://npm.taobao.org/mirrors/electron/ # electron 二进制包镜像
+  npm set puppeteer_download_host https://npm.taobao.org/mirrors # puppeteer 二进制包镜像
+  npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver # chromedriver 二进制包镜像
+  npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver # operadriver 二进制包镜像
+  npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs # phantomjs 二进制包镜像
+  npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium # selenium 二进制包镜像
+  npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector # node-inspector 二进制包镜像
+  npm set canvas_binary_host_mirror=https://npm.taobao.org/mirrors/node-canvas-prebuilt # node-canvas 二进制包镜像
+  npm cache verify # 清空缓存
 }
 
 # 安装 Homebrew
@@ -79,28 +94,16 @@ fi
 if command_exists node; then
   echo "node 版本：$(node --version)"
 
+  # 检查是否设置为淘宝源
   npmRegistry=`npm config get registry`
   if [ $npmRegistry != "https://registry.npm.taobao.org/" ]; then
-    echo 'ok'
-  else
-    echo 'fail'
+    npmConfig
   fi
 else
   brew install node
 
-  # 设置淘宝源
-  npm set registry https://registry.npm.taobao.org # 注册模块镜像
-  npm set disturl https://npm.taobao.org/dist # node-gyp 编译依赖的 node 源码镜像
-  npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass # node-sass 二进制包镜像
-  npm set electron_mirror https://npm.taobao.org/mirrors/electron/ # electron 二进制包镜像
-  npm set puppeteer_download_host https://npm.taobao.org/mirrors # puppeteer 二进制包镜像
-  npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver # chromedriver 二进制包镜像
-  npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver # operadriver 二进制包镜像
-  npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs # phantomjs 二进制包镜像
-  npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium # selenium 二进制包镜像
-  npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector # node-inspector 二进制包镜像
-  npm set canvas_binary_host_mirror=https://npm.taobao.org/mirrors/node-canvas-prebuilt # node-canvas 二进制包镜像
-  npm cache verify # 清空缓存
+  # 修改 npm 配置
+  npmConfig
 fi
 
 # 安装 yarn
